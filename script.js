9cc5676dd17ed7c617ec2a80788261420cf5c718
Jongita <jurgitajonaityte@gmail.com>
2025-02-10 18:14:30 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navToggle = document.querySelector(".nav-toggle");
+    const navMenu = document.querySelector(".mainHeaderList");
+
+    if (navToggle) {
+        navToggle.addEventListener("click", function () {
+            navMenu.classList.toggle("active");
+        });
+    } else {
+        console.error("Hamburger button (.nav-toggle) not found!");
+    }
+});
+
