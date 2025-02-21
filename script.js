b39189ccd33cccfd1eb36cc1d24dc8b09e824eae
Jongita <jurgitajonaityte@gmail.com>
2025-02-21 10:18:58 +0200

    contacts

--- script.js
+++ script.js
@@ -98,4 +98,12 @@ window.onload = function () {
     displayProducts();
 };
 
-
+// footer:
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-placeholder").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading footer:", error));
+});
